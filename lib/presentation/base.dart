28e3269bf2581b34f687c45cf980602443424a4a
augustosalazar <augustosalazar@uninorte.edu.co>
2021-04-23 22:11:02 -0500

    very basic structure

---
+++ lib/presentation/base.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'main/home_page.dart';
+import 'theme/theme.dart';
+
+class Base extends StatelessWidget {
+  const Base({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: MyTheme.darkTheme,
+        //home: Scaffold(),
+        home: HomePage());
+  }
+}
