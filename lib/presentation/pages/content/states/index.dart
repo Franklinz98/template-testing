7b8917e8b40c5c4c8c1fa482ad236e75aa877218
Franklinz98 <franklinz@uninorte.edu.co>
2021-05-13 11:22:18 -0500

    Comments

--- lib/presentation/pages/content/states/index.dart
+++ lib/presentation/pages/content/states/index.dart
@@ -1,31 +1,23 @@
 import './widgets/state_card.dart';
 import 'package:flutter/material.dart';
 
-class UsersStates extends StatefulWidget {
+class UsersStates extends StatelessWidget {
+
   // UsersStates empty constructor
   UsersStates({Key? key}) : super(key: key);
 
   @override
-  _State createState() => _State();
-}
-
-// UsersStates custom state
-class _State extends State<UsersStates> {
-  final items = List<String>.generate(20, (i) => "Item $i");
-
-  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
         // StateCard usage example
-        return StateCard(
-          title: 'Iony Houst',
-          content: 'Lorem ipsum dolor sit amet.',
-          picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
-          onChat: () => {},
-        );
-      },
+        StateCard(
+            title: 'Iony Houst',
+            content: 'Lorem ipsum dolor sit amet.',
+            picUrl: 'https://uifaces.co/our-content/donated/gPZwCbdS.jpg',
+            onChat: () => {}),
+      ],
     );
   }
 }
--- lib/presentation/pages/content/users_offers/index.dart
+++ lib/presentation/pages/content/users_offers/index.dart
@@ -1,33 +1,26 @@
 import './widgets/offer_card.dart';
 import 'package:flutter/material.dart';
 
-class UsersOffers extends StatefulWidget {
+class UsersOffers extends StatelessWidget {
+
   // UsersOffers empty constructor
   UsersOffers({Key? key}) : super(key: key);
 
   @override
-  _State createState() => _State();
-}
-
-// UsersOffers custom state
-class _State extends State<UsersOffers> {
-  final items = List<String>.generate(20, (i) => "Item $i");
-
-  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
         // PostCard usage example
-        return PostCard(
+        PostCard(
           title: 'Ludvig Wiese',
           content:
               'Duis non tellus sed quam luctus gravida quis sed libero. Pellentesque luctus lorem eu est varius, eu dignissim leo tincidunt. Fusce eget ante sed mi venenatis tincidunt et rutrum neque. Suspendisse laoreet sapien sed est aliquet fringilla. Fusce fringilla, ante in ultrices volutpat, mauris ',
           picUrl:
               'https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200',
           onChat: () => {},
-        );
-      },
+        ),
+      ],
     );
   }
 }
