d43d40c75f7f0a943987bc46e1f066ea06cecf49
Franklin E. Zabaleta <franklinz@uninorte.edu.co>
2021-07-29 08:30:10 -0500

    test

--- lib/domain/use_case/controller.dart
+++ lib/domain/use_case/controller.dart
@@ -2,14 +2,6 @@ import 'package:get/get.dart';
 
 class Controller extends GetxController {
   // Observables
-  var currentUser = false.obs;
-  var screenIndex = 0.obs;
 
-  void updateUser(bool userAuth) {
-    currentUser.value = userAuth;
-  }
-
-  void updateScreenIndex(int index) {
-    screenIndex.value = index;
-  }
+  // TODO add controller variables
 }
